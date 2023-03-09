9a9c5f964b5cfa2a558db5e9ed383f6239934a32
zhaolei <979569187@qq.com>
2023-03-09 09:37:55 +0800

    关于request

--- knowledge/about_request.py
+++ knowledge/about_request.py
@@ -1,7 +1,7 @@
 import requests
 
-url = "https://api.dstcar.com/sso/sendVerifCode"
-send_head = "'phone':'15070720246'"
+url = "http://store.test.dstcar.com/dst-partner-apis/api-store/public/verifyCode"
+send_head = {'phone':'15070720246'}
 
 a = requests.post(url,data="",json=send_head)
 print(a.json())
