2d25121fc039417098465ec50a3d6e614e6e3152
zhaolei <979569187@qq.com>
2023-03-11 14:28:18 +0800

    关于request

--- knowledge/about_request.py
+++ knowledge/about_request.py
@@ -1,7 +1,19 @@
 import requests
 
 url = "http://store.test.dstcar.com/dst-partner-apis/api-store/public/verifyCode"
-send_head = {'phone':'15070720246'}
+send_data = {'phone':'15070720246'}
 
-a = requests.post(url,data="",json=send_head)
+a = requests.post(url,data=send_data)
 print(a.json())
+
+url1 = "http://store.test.dstcar.com/dst-partner-apis/api-store/public/login"
+send_data = {"phone":"15070720246","verifyCode":"999999"}
+b = requests.post(url1,data=send_data)
+get_token = b.json()["data"]["token"]
+print(b.json()["data"]["token"])
+
+url2 = "http://store.test.dstcar.com/dst-partner-apis/api-store/store/user/getUserInfo"
+send_header = {"token": get_token}
+print(send_header)
+b = requests.post(url2,headers=send_header)
+print(b.json())
